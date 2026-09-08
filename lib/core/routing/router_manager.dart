998e82cafaa0e56d5b1aac9af5fcb9b51f93a436
ahmedalaayq <ahmed.alaayq@gmail.com>
2026-09-08 13:57:55 +0300

    Implement signup feature with UI components, routing, and navigation extensions

--- lib/core/routing/router_manager.dart
+++ lib/core/routing/router_manager.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:mini_shop/core/routing/app_routes.dart';
 import 'package:mini_shop/core/routing/no_route_found.dart';
 import 'package:mini_shop/features/auth/login/ui/views/login_view.dart';
+import 'package:mini_shop/features/auth/signup/ui/views/signup_view.dart';
 
 class RoutingManager {
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case AppRoutes.loginView:
         return MaterialPageRoute(builder: (_) => LoginView());
+      case AppRoutes.signupView:
+        return MaterialPageRoute(builder: (_) => SignupView());
       default:
         return MaterialPageRoute(builder: (_) => NoRouteFoundView());
     }
