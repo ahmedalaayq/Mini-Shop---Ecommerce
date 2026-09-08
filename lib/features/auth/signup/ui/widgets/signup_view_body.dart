bcdf5e7fa846936d5bfd5daaf087f80b37f05d67
ahmedalaayq <ahmed.alaayq@gmail.com>
2026-09-08 17:14:28 +0300

    Add password strength widget to signup view with validation feedback

--- lib/features/auth/signup/ui/widgets/signup_view_body.dart
+++ lib/features/auth/signup/ui/widgets/signup_view_body.dart
@@ -4,6 +4,7 @@ import 'package:mini_shop/core/helpers/spacing.dart';
 import 'package:mini_shop/core/theme/app_styles.dart';
 import 'package:mini_shop/core/widgets/app_elevated_button.dart';
 import 'package:mini_shop/core/widgets/app_text_form_field.dart';
+import 'package:mini_shop/features/auth/signup/ui/widgets/password_strength.dart';
 
 class SignupViewBody extends StatefulWidget {
   const SignupViewBody({super.key});
@@ -19,12 +20,32 @@ class _SignupViewBodyState extends State<SignupViewBody> {
   final TextEditingController _confirmPasswordController =
       TextEditingController();
 
+  final ValueNotifier<String> _passwordNotifier = ValueNotifier('');
+  final ValueNotifier<String> _confirmPasswordNotifier = ValueNotifier('');
+
+  @override
+  void initState() {
+    super.initState();
+
+    _passwordController.addListener(() {
+      _passwordNotifier.value = _passwordController.text;
+    });
+
+    _confirmPasswordController.addListener(() {
+      _confirmPasswordNotifier.value = _confirmPasswordController.text;
+    });
+  }
+
   @override
   void dispose() {
     _nameController.dispose();
     _emailController.dispose();
     _passwordController.dispose();
     _confirmPasswordController.dispose();
+
+    _passwordNotifier.dispose();
+    _confirmPasswordNotifier.dispose();
+
     super.dispose();
   }
 
@@ -43,55 +64,75 @@ class _SignupViewBodyState extends State<SignupViewBody> {
               style: AppStyles.readexPro16Weight400,
             ),
             verticalSpace(24),
-            Column(
-              crossAxisAlignment: .start,
-              children: [
-                Text(
-                  'Full Name',
-                  style: TextStyle(fontSize: 16.sp, fontWeight: .w600),
-                ),
-                verticalSpace(4),
-                AppTextFormField(
-                  hintText: 'Enter your full name',
-                  controller: _nameController,
-                ),
-                verticalSpace(16),
-                Text(
-                  'Email',
-                  style: TextStyle(fontSize: 16.sp, fontWeight: .w600),
-                ),
-                verticalSpace(4),
-                AppTextFormField(
-                  hintText: 'Enter your email address',
-                  controller: _emailController,
-                ),
-                verticalSpace(16),
-                Text(
-                  'Password',
-                  style: TextStyle(fontSize: 16.sp, fontWeight: .w600),
-                ),
-                verticalSpace(4),
-                AppTextFormField(
-                  isPassword: true,
-                  hintText: 'Enter your password',
-                  controller: _passwordController,
-                ),
-                verticalSpace(24),
 
-                Text(
-                  'Confirm Password',
-                  style: TextStyle(fontSize: 16.sp, fontWeight: .w600),
-                ),
-                verticalSpace(4),
-                AppTextFormField(
-                  isPassword: true,
-                  hintText: 'Enter your confirm password',
-                  controller: _confirmPasswordController,
-                ),
-                verticalSpace(55),
-                AppElevatedButton(btnText: 'Create Account', onPressed: () {}),
-              ],
+            Text(
+              'Full Name',
+              style: TextStyle(fontSize: 16.sp, fontWeight: .w600),
             ),
+            verticalSpace(4),
+            AppTextFormField(
+              hintText: 'Enter your full name',
+              controller: _nameController,
+            ),
+
+            verticalSpace(16),
+
+            Text(
+              'Email',
+              style: TextStyle(fontSize: 16.sp, fontWeight: .w600),
+            ),
+            verticalSpace(4),
+            AppTextFormField(
+              hintText: 'Enter your email address',
+              controller: _emailController,
+            ),
+
+            verticalSpace(16),
+
+            Text(
+              'Password',
+              style: TextStyle(fontSize: 16.sp, fontWeight: .w600),
+            ),
+            verticalSpace(4),
+            AppTextFormField(
+              isPassword: true,
+              hintText: 'Enter your password',
+              controller: _passwordController,
+            ),
+
+            verticalSpace(24),
+
+            Text(
+              'Confirm Password',
+              style: TextStyle(fontSize: 16.sp, fontWeight: .w600),
+            ),
+            verticalSpace(4),
+            AppTextFormField(
+              isPassword: true,
+              hintText: 'Enter your confirm password',
+              controller: _confirmPasswordController,
+            ),
+
+            verticalSpace(24),
+
+            ValueListenableBuilder<String>(
+              valueListenable: _passwordNotifier,
+              builder: (context, password, _) {
+                return ValueListenableBuilder<String>(
+                  valueListenable: _confirmPasswordNotifier,
+                  builder: (context, confirmPassword, _) {
+                    return PasswordStrengthWidget(
+                      password: password.trim(),
+                      confirmPassword: confirmPassword.trim(),
+                    );
+                  },
+                );
+              },
+            ),
+
+            verticalSpace(55),
+
+            AppElevatedButton(btnText: 'Create Account', onPressed: () {}),
           ],
         ),
       ),
