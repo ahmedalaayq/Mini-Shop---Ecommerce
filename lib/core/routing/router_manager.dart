ec7339a52e70bec842d618648779d16386ffc3a3
ahmedalaayq <ahmed.alaayq@gmail.com>
2026-09-08 13:41:26 +0300

    Implement login feature with UI components and routing - add spacing helper file - add core button and text form field

--- lib/core/routing/router_manager.dart
+++ lib/core/routing/router_manager.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:mini_shop/core/routing/app_routes.dart';
 import 'package:mini_shop/core/routing/no_route_found.dart';
+import 'package:mini_shop/features/auth/login/ui/views/login_view.dart';
 
 class RoutingManager {
-  Route<dynamic> onGenerateRoute(RouteSettings settings) {
+  static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
+      case AppRoutes.loginView:
+        return MaterialPageRoute(builder: (_) => LoginView());
       default:
         return MaterialPageRoute(builder: (_) => NoRouteFoundView());
     }
--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-class AppColors 
-{
+class AppColors {
   static const Color primaryBlue = Color(0xFF3669C9);
+  static const Color black = Color(0xFF1A1A1A);
+  static const Color grey = Color(0xFF808080);
 }
