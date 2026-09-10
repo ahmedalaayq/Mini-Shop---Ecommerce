716839caf138e48d0a39752d8bcfcd072fa785ac
ahmedalaayq <ahmed.alaayq@gmail.com>
2026-09-10 22:50:24 +0300

    refactor app sizes extension

--- lib/core/extensions/app_sizes_extension.dart
+++ lib/core/extensions/app_sizes_extension.dart
@@ -8,12 +8,4 @@ extension AppSizesExtension on num {
   double get sp => ScreenUtil().setSp(toDouble());
 
   double get r => ScreenUtil().radius(toDouble());
-
-  static double width(double width) => ScreenUtil().setWidth(width);
-
-  static double height(double height) => ScreenUtil().setHeight(height);
-
-  static double fontSize(double size) => ScreenUtil().setSp(size);
-
-  static double radius(double radius) => ScreenUtil().radius(radius);
 }
